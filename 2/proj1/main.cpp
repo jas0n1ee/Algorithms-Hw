fbd1f5c0b6c8eb1c55e74d6d25e0100fa21edf1f
jasonlee <i@jas0n1ee.me>
2013-10-02 17:37:21 +0800

    it seems ok with the input

--- 2/proj1/main.cpp
+++ 2/proj1/main.cpp
@@ -21,11 +21,15 @@ void CNode_init(CNode *t)
 		t[i].next=&t[i+1];
 		t[i].col=0;
 		t[i].row=0;
+		t[i].right=NULL;
+		t[i].down=NULL;
 	}
 	t[0].next=&t[1];
-	t[N].next=&t[0];
+	t[N].next=NULL;
 	t[N].col=0;
 	t[N].row=0;
+	t[N].right=NULL;
+	t[N].down=NULL;
 	return;
 }
 void print_matrix(CNode *t)
@@ -44,15 +48,96 @@ void print_matrix(CNode *t)
 		}
 	}
 }
+void build_list(CNode *t,CrosLNode *x,int m)
+{
+	int N=t[0].col;
+	for(int i=0;i<m;i++)
+	{
+		int row=x[i].row,col=x[i].col;
+		if(t[row+1].right==NULL) 
+		{
+			t[row+1].right=&x[i];
+			x[i].right=NULL;
+		}
+		else 
+		{
+			CrosLNode *point=t[row+1].right;
+			if((*point).col>col) 
+			{
+				x[i].right=point;
+				t[row+1].right=&x[i];
+			}
+			else
+				do
+				{
+					if((*(*point).right).col>col)  
+					{
+						x[i].right=(*point).right;
+						(*point).right=&x[i];
+						break;
+					}
+					if((*point).right==NULL&&col>(*point).col) 
+					{
+						(*point).right=&x[i];
+						x[i].right=NULL;
+						break;
+					}
+					point=(*point).right;
+				}while((*point).right!=NULL);
+		}
+				
+	}
+	for(int i=0;i<m;i++)
+	{
+		int row=x[i].row,col=x[i].col;
+		if(t[col+1].down==NULL) 
+		{
+			t[col+1].down=&x[i];
+			x[i].down=NULL;
+		}
+		else 
+		{
+			CrosLNode *point=t[col+1].down;
+			if((*point).row>row) 
+			{
+				x[i].down=point;
+				t[col+1].down=&x[i];
+			}
+			else
+				do
+				{
+					if((*(*point).down).row>row)  
+					{
+						x[i].down=(*point).down;
+						(*point).down=&x[i];
+						break;
+					}
+					if((*point).down==NULL&&row>(*point).row) 
+					{
+						(*point).down=&x[i];
+						x[i].down=NULL;
+						break;
+					}
+					point=(*point).down;
+				}while((*point).down!=NULL);
+		}
+				
+	}
+}
 int main()
 {
 	int N,m;
 	cin>>N>>m;
 	CNode *HEAD_A=new CNode[N+1];
+	CNode *HEAD_B=new CNode[N+1];
+	CNode *HEAD_C=new CNode[N+1];
 	CrosLNode *Matrix_A=new CrosLNode[m];
+	CrosLNode *Matrix_B=new CrosLNode[m];
+	CrosLNode *Matrix_C=new CrosLNode[m];
 	HEAD_A[0].col=N;
 	HEAD_A[0].row=N;
 	CNode_init(HEAD_A);
+	/*
 	HEAD_A[1].next=NULL;
 	HEAD_A[1].right=&Matrix_A[0];
 	HEAD_A[1].down=&Matrix_A[0];
@@ -62,11 +147,15 @@ int main()
 	Matrix_A[0].down=NULL;
 	Matrix_A[0].right=NULL;
 	print_matrix(HEAD_A);
-	/*
+	*/
 	for(int i=0;i<m;i++)
 	{
-		
-	*/
+		cin>>Matrix_A[i].row>>Matrix_A[i].col>>Matrix_A[i].val;
+		Matrix_A[i].right=NULL;
+		Matrix_A[i].down=NULL;
+	}
+	build_list(HEAD_A,Matrix_A,m);
+	print_matrix(HEAD_A);
 	return 0;
 }
 	
