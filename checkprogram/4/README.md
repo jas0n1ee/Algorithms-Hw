556c1079328a01ba0d42f9ba8e92f1db1e5df51e
jas0n1ee <i@jas0n1ee.me>
2013-11-04 20:22:14 +0800

    checkprogram 4

---
+++ checkprogram/4/README.md
@@ -0,0 +1,32 @@
+README
+-----
+将自己写的数算4作业生成的`main_1.exe` `main_2.exe`（重命名一下）拷到文件目录下
+win8 64位系统可以直接执行`check.exe`  
+其余的。。。自编译一下`check.cpp`，生成的.exe文件也要在此目录下
+  
+如果不出错的话...应该会生成一个`result.txt`的文件，里面就是结果
+正确Right 错误Wrong
+
+-----
+#结果说明
+		--------------------------test1_1----------------------------
+		Keywords:	Right
+		Sum:		Right
+		Level:		Right
+前面的数字表示第几组测试数据  
+`Keyword`为对关键字序列的识别结果  
+`Sum`为子树结点数目的识别结果   
+`Level`为层数的识别结果  
+
+		--------------------------test2_1----------------------------
+		Result:		Right
+		SUM:		Right
+		Level:		Right
+`Result`为判断的识别结果，若表达结果为`NO`则不会有`SUM`和`Level`的判断  
+`SUM`为层数个数的判断  
+`Level`为所有层数结果的判断
+
+-----
+
+以上
+By [jas0n1ee](mailto:lijisheng@thuee25.cn)
---
+++ checkprogram/4/check.cpp
@@ -0,0 +1,119 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<cstdlib>
+#include<windows.h> 
+using namespace std;
+void get(char &t,fstream &in)
+{
+	t=in.get();
+	if(t==' ') get(t,in);
+}
+int main()
+{
+	SYSTEMTIME sys;
+	int min,sec,ms;
+	int count=0;
+	char rout1[10]="a1_1.txt";
+	char rout2[10]="o1_1.txt";
+	char com[40]="main_1.exe i_1.txt o1_1.txt";
+	fstream out("result.txt",ios::out|ios::trunc);
+	for(int i=1;i<5;i++)
+	{
+		out<<"--------------------------test1_"<<i<<"----------------------------\n";
+		rout1[3]=i+48;
+		rout2[3]=i+48;
+		com[13]=i+48;
+		com[22]=i+48;
+		min=sys.wMinute;
+		sec=sys.wSecond;
+		ms=sys.wMilliseconds;
+		system(com);
+		//cout<<system(com); 
+		//cout<<sys.wMinute<<sys.wSecond-sec<<sys.wMilliseconds-ms<<endl;
+		fstream r(rout1,ios::in);
+		fstream t(rout2,ios::in);
+		//if(!(r&&t)) break;
+		char t1,t2;
+		bool flag=1;
+		while(1)
+		{
+			get(t1,r);
+			get(t2,t);
+			if(t1!=t2) flag=0;
+			if(t1=='\n'&&t2=='\n') break;
+		}
+		out<<"Keywords:\t";
+		if(flag)out<<"Right\n";
+		else out<<"Wrong\n";
+		flag=1;
+		r.get();t.get();
+		while(1)
+		{
+			get(t1,r);
+			get(t2,t);
+			if(t1!=t2) flag=0;
+			if(t1=='\n'&&t2=='\n') break;
+		}
+		out<<"Sum:\t\t";
+		if(flag)out<<"Right\n";
+		else out<<"Wrong\n";
+		flag=1;
+		int t3,t4;
+		r>>t3;t>>t4;
+		out<<"Level:\t\t";
+		if(t3==t4) out<<"Right\n";
+		else out<<"Wrong\n";
+		r.close();
+		t.close();
+	}
+	rout1[1]='2';
+	rout2[1]='2';
+	char com2[40]="main_2.exe p_1.txt c_1.txt o2_1.txt";
+	for(int i=1;i<4;i++)
+	{
+		out<<"--------------------------test2_"<<i<<"----------------------------\n";
+		rout1[3]=i+48;
+		rout2[3]=i+48;
+		com2[13]=i+48;
+		com2[21]=i+48;
+		com2[30]=i+48;
+		system(com2);
+		//cout<<system(com); 
+		//cout<<sys.wMinute<<sys.wSecond-sec<<sys.wMilliseconds-ms<<endl;
+		fstream r(rout1,ios::in);
+		fstream t(rout2,ios::in);
+		char temp[5],temp1[5];
+		int x1,x2,n;
+		r>>temp;
+		t>>temp1;
+		out<<"Result:\t\t";
+		if(strcmp(temp,temp1)==0) out<<"Right\n";
+		else out<<"Wrong\n";
+		bool flag=1;
+		if(temp[0]=='Y')
+		{
+			r>>x1;
+			t>>x2;
+			out<<"SUM:\t\t";
+			if(x1!=x2)out<<"Wrong\n";
+			else
+			{
+				out<<"Right\n";
+				n=x1;
+				for(int i=0;i<n;i++)
+				{
+					r>>x1;
+					t>>x2;
+					if(x1!=x2) flag=0;
+				}
+			}
+			out<<"Level:\t\t";
+			if(flag) out<<"Right\n";
+			else out<<"Wrong\n";
+		}
+		
+	}
+	out<<"-------------------------------------------------------------\n";
+	out.close();
+}
