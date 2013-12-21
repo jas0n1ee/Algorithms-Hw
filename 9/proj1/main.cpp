1802209e53b41a8dd951a5e1ff84b29006bb1b0a
jas0n1ee <i@jas0n1ee.me>
2013-12-22 01:17:22 +0800

    finish

--- 9/proj1/main.cpp
+++ 9/proj1/main.cpp
@@ -2,37 +2,41 @@
 using namespace std;
 static int N;
 static int *arr;
-static int *b;
-static int *c;
-int search(int num, int k)
+static int *L,*M,*pre;
+int search(int *a,int num, int k)
 {
- 	int low=0, high=k;
+ 	int low=0, high=k-1;
  	while(low<=high)
  	{
 		int mid=(low+high)/2;
- 		if(num>b[mid])
+ 		if(num>a[mid])
  		low=mid+1;
  		else
 		high=mid-1;
 	}
 	return low;
 } 
-int LIS()
+int LIS(int *t)
 {
-	int k=0;
-	b[0]=arr[0];
-	c[0]=arr[0];
-	for(int i=1;i<=N;i++)
+	int k=1,j=0;
+	L[0]=arr[0];
+	M[0]=0;
+	pre[0]=-1;
+	for(int i=1;i<N;i++)
 	{
-		if(arr[i]>b[k]) 
-		{
-			b[++k]=arr[i];
-			c[k]=arr[i];
-		}
-		else
-		{
-			b[search(arr[i],k)+1]=arr[i];
-		}
+		j=search(L,arr[i],k);
+		L[j]=arr[i];
+		M[j]=i;
+		if(j>0) pre[i]=M[j-1];
+		else pre[i]=-1;
+		if(j+1>k) k++;
+	}
+	cout<<k<<endl;
+	j=M[k-1];
+	for(int i=k-1;i>=0&&j!=-1;i--)
+	{
+		t[i]=arr[j];
+		j=pre[j];
 	}
 	return k;
 }
@@ -40,14 +44,13 @@ int main()
 {
 	cin>>N;
 	arr=new int[N];
-	b=new int[N];
-	c=new int[N];
+	M=new int[N];
+	L=new int[N];
+	pre=new int[N];
+	int *t=new int [N];
 	for(int i=0;i<N;i++) cin>>arr[i];
-	int a=LIS();
-	cout<<a<<endl;
-	for(int i=0;i<a;i++) cout<<c[i]<<" ";
-	cout<<endl;
+	int m=LIS(t);
+	for(int i=0;i<m;i++) cout<<t[i]<<" ";
 	delete []arr;
-	delete []b;
-	delete []c;
+	delete []M;
 }
